e185a42ce3ac803af4e48160054fc3930a15ff5a
blapete <pjblank2@gmail.com>
2020-11-09 10:00:12 -0500

    block element modifier

--- app/static/js/index.js
+++ app/static/js/index.js
@@ -1,48 +1,34 @@
-const dayThemeColor = "rgb(238, 238, 238)";
-const nightThemeColor = "rgba(28, 43, 45, 0.9)";
-const lightMoonImage = document.getElementById("nightTheme__Image");
-const darkMoonImage = document.getElementById("dayTheme__Image");
-const baseTemplateBlock = document.getElementById("base__Template");
-const chemTemplateArrowImage = document.getElementById("chem__Image");
-const chemTemplateCarousel = document.getElementById("chem__CarouselSection");
-const dailyTemplatetable = document.querySelector("table");
-const dailyTemplateData = document.querySelectorAll("td");
-const dailyTemplateDayToggler = document.querySelectorAll(".daily-button-day");
-const dailyTemplateNightToggler = document.querySelectorAll(
-  ".daily-button-night"
-);
-const homeTemplateAtomImage = document.getElementById("home__Atom");
-const homeTemplateFlaskImage = document.getElementById("home__Image");
-const industryTemplateGridItems = document.querySelectorAll(".grid-item");
-const globalAnchorTags = document.querySelectorAll("a");
-const reactorTemplateImage = document.getElementById("alchemy__Image");
-const reactorTemplateDropdownOne = document.querySelector(
-  ".reactor__SelectBox1"
-);
-const reactorTemplateDropdownTwo = document.querySelector(
-  ".reactor__SelectBox2"
-);
-const reactorTemplateOptionsOne = document.querySelector(
-  ".reactor__OptionsContainer1"
-);
-const reactorTemplateOptionsTwo = document.querySelector(
-  ".reactor__OptionsContainer2"
-);
-const reactorTemplateInputOne = document.querySelector(
-  ".reactor__SearchBox1 input"
-);
-const reactorTemplateInputTwo = document.querySelector(
-  ".reactor__SearchBox2 input"
-);
-let reactorTemplateSelectedOne = document.querySelector(".reactor__Selected1");
-let reactorTemplateSelectedTwo = document.querySelector(".reactor__Selected2");
+const dayTheme = "rgb(238, 238, 238)";
+const nightTheme = "rgba(28, 43, 45, 0.9)";
+const lightMoon = document.querySelector(".theme__link--night");
+const darkMoon = document.querySelector(".theme__link--day");
+const baseTemplate = document.querySelector(".base");
+const arrowIcon = document.querySelector(".chem-image");
+const elementCarousel = document.querySelector(".chem__carousel");
+const dailyTable = document.querySelector("table");
+const dailyData = document.querySelectorAll("td");
+const tempToggleDay = document.querySelectorAll(".button--day");
+const tempToggleNight = document.querySelectorAll(".button--night");
+const atomIcon = document.querySelector(".home-atom");
+const flaskIcon = document.querySelector(".home-flask");
+const industryGrid = document.querySelectorAll(".industry-column");
+const anchorTags = document.querySelectorAll("a");
+const alchemyIcon = document.querySelector(".head__image");
+const dropdownOneStatic = document.querySelector(".dropdown-1__select");
+const dropdownTwoStatic = document.querySelector(".dropdown-2__select");
+const dropdownOneOptions = document.querySelector(".dropdown-1__options");
+const dropdownTwoOptions = document.querySelector(".dropdown-2__options");
+const dropdownOneInput = document.querySelector(".dropdown-1__search input");
+const dropdownTwoInput = document.querySelector(".dropdown-2__search input");
+let dropdownOneSelected = document.querySelector(".dropdown-1__selected");
+let dropdownTwoSelected = document.querySelector(".dropdown-2__selected");
 
 document.addEventListener("DOMContentLoaded", () => {
   if (localStorage.getItem("theme")) {
     let saved_theme = localStorage.getItem("theme");
-    if (saved_theme === dayThemeColor) {
+    if (saved_theme === dayTheme) {
       goDayTheme();
-    } else if (saved_theme === nightThemeColor) {
+    } else if (saved_theme === nightTheme) {
       goNightTheme();
     }
   }
@@ -50,192 +36,191 @@ document.addEventListener("DOMContentLoaded", () => {
 
 const toggleTheme = () => {
   const backdropComputedColor = getComputedStyle(
-    baseTemplateBlock,
+    baseTemplate,
     null
   ).getPropertyValue("background-color");
   console.log(
     `theme is toggling => current theme is ${
-      backdropComputedColor === dayThemeColor ? "day" : "night"
+      backdropComputedColor === dayTheme ? "day" : "night"
     }, new theme is going to be ${
-      backdropComputedColor === dayThemeColor ? "night" : "day"
+      backdropComputedColor === dayTheme ? "night" : "day"
     }`
   );
-  if (backdropComputedColor === dayThemeColor) {
+  if (backdropComputedColor === dayTheme) {
     goNightTheme();
-  } else if (backdropComputedColor === nightThemeColor) {
+  } else if (backdropComputedColor === nightTheme) {
     goDayTheme();
   }
 };
 
 const goDayTheme = () => {
   document.body.style.color = "black";
+  if (elementCarousel) {
+    let chemSquares = document.querySelectorAll(".carousel__content");
 
-  if (chemTemplateCarousel) {
-    let chemSquares = document.querySelectorAll(".chem__CarouselNightContent");
     if (chemSquares.length > 0) {
       for (let i of chemSquares) {
-        i.classList.remove("chem__CarouselNightContent");
-        i.classList.add("chem__CarouselDayContent");
+        i.classList.remove("carousel__content--night");
+        i.classList.add("carousel__content--day");
       }
     }
   }
 
-  for (let a of globalAnchorTags) {
+  for (let a of anchorTags) {
     a.style.color = "black";
   }
 
-  if (dailyTemplatetable) {
-    dailyTemplatetable.style.border = "1px solid black";
-    for (let td of dailyTemplateData) {
+  if (dailyTable) {
+    dailyTable.style.border = "1px solid black";
+    for (let td of dailyData) {
       td.style.border = "1px solid black";
     }
   }
 
-  darkMoonImage.style.display = "";
-  lightMoonImage.style.display = "none";
+  darkMoon.style.display = "";
+  lightMoon.style.display = "none";
 
-  if (homeTemplateFlaskImage) {
-    homeTemplateFlaskImage.style.backgroundColor = "";
-    homeTemplateFlaskImage.style.padding = "";
-    homeTemplateFlaskImage.style.borderRadius = "";
+  if (flaskIcon) {
+    flaskIcon.style.backgroundColor = "";
+    flaskIcon.style.padding = "";
+    flaskIcon.style.borderRadius = "";
   }
 
-  if (industryTemplateGridItems) {
-    for (let grid of industryTemplateGridItems) {
+  if (industryGrid) {
+    for (let grid of industryGrid) {
       grid.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
     }
   }
 
-  if (dailyTemplateDayToggler.length) {
-    dailyTemplateDayToggler.forEach((item) => {
+  if (tempToggleDay.length) {
+    tempToggleDay.forEach((item) => {
       item.style.display = "";
     });
   }
 
-  if (dailyTemplateNightToggler.length) {
-    dailyTemplateNightToggler.forEach((item) => {
+  if (tempToggleNight.length) {
+    tempToggleNight.forEach((item) => {
       item.style.display = "none";
     });
   }
 
-  if (reactorTemplateImage) {
-    reactorTemplateImage.style.backgroundColor = "";
-    reactorTemplateImage.style.borderRadius = "";
+  if (alchemyIcon) {
+    alchemyIcon.style.backgroundColor = "";
+    alchemyIcon.style.borderRadius = "";
   }
 
-  if (homeTemplateAtomImage) {
-    homeTemplateAtomImage.style.backgroundColor = "";
-    homeTemplateAtomImage.style.padding = "";
-    homeTemplateAtomImage.style.borderRadius = "";
+  if (atomIcon) {
+    atomIcon.style.backgroundColor = "";
+    atomIcon.style.padding = "";
+    atomIcon.style.borderRadius = "";
   }
 
-  if (chemTemplateArrowImage) {
-    chemTemplateArrowImage.style.backgroundColor = "";
-    chemTemplateArrowImage.style.padding = "";
-    chemTemplateArrowImage.style.borderRadius = "";
+  if (arrowIcon) {
+    arrowIcon.style.backgroundColor = "";
+    arrowIcon.style.padding = "";
+    arrowIcon.style.borderRadius = "";
   }
 
-  if (reactorTemplateDropdownOne && reactorTemplateDropdownTwo) {
-    reactorTemplateOptionsOne.style.backgroundColor = "rgba(0,0,0,0.6)";
-    reactorTemplateOptionsOne.style.color = "white";
-    reactorTemplateOptionsTwo.style.backgroundColor = "rgba(0,0,0,0.6)";
-    reactorTemplateOptionsTwo.style.color = "white";
+  if (dropdownOneStatic && dropdownTwoStatic) {
+    dropdownOneOptions.style.backgroundColor = "rgba(0,0,0,0.6)";
+    dropdownOneOptions.style.color = "white";
+    dropdownTwoOptions.style.backgroundColor = "rgba(0,0,0,0.6)";
+    dropdownTwoOptions.style.color = "white";
   }
 
-  baseTemplateBlock.style.backgroundColor = dayThemeColor;
+  baseTemplate.style.backgroundColor = dayTheme;
 
   window.localStorage.clear();
-  window.localStorage.setItem("theme", dayThemeColor);
+  window.localStorage.setItem("theme", dayTheme);
 };
 
 const goNightTheme = () => {
   document.body.style.color = "white";
 
-  for (let a of globalAnchorTags) {
+  for (let a of anchorTags) {
     a.style.color = "white";
   }
 
-  if (dailyTemplatetable) {
-    dailyTemplatetable.style.border = "1px solid white";
-    for (let td of dailyTemplateData) {
+  if (dailyTable) {
+    dailyTable.style.border = "1px solid white";
+    for (let td of dailyData) {
       td.style.border = "1px solid white";
     }
   }
 
-  lightMoonImage.style.display = "";
-  darkMoonImage.style.display = "none";
+  lightMoon.style.display = "";
+  darkMoon.style.display = "none";
 
-  if (homeTemplateFlaskImage) {
-    homeTemplateFlaskImage.style.backgroundColor = "white";
-    homeTemplateFlaskImage.style.padding = "2rem";
-    homeTemplateFlaskImage.style.borderRadius = "25%";
+  if (flaskIcon) {
+    flaskIcon.style.backgroundColor = "white";
+    flaskIcon.style.padding = "2rem";
+    flaskIcon.style.borderRadius = "25%";
   }
 
-  if (industryTemplateGridItems) {
-    for (let grid of industryTemplateGridItems) {
+  if (industryGrid) {
+    for (let grid of industryGrid) {
       grid.style.backgroundColor = "rgba(255, 255, 255, 0.2)";
     }
   }
 
-  if (dailyTemplateNightToggler.length) {
-    dailyTemplateNightToggler.forEach((item) => {
+  if (tempToggleNight.length) {
+    tempToggleNight.forEach((item) => {
       item.style.display = "";
     });
   }
 
-  if (dailyTemplateDayToggler.length) {
-    dailyTemplateDayToggler.forEach((item) => {
+  if (tempToggleDay.length) {
+    tempToggleDay.forEach((item) => {
       item.style.display = "none";
     });
   }
 
-  if (reactorTemplateImage) {
-    reactorTemplateImage.style.backgroundColor = "white";
-    reactorTemplateImage.style.borderRadius = "50%";
+  if (alchemyIcon) {
+    alchemyIcon.style.backgroundColor = "white";
+    alchemyIcon.style.borderRadius = "50%";
   }
 
-  if (chemTemplateCarousel) {
-    let chemSquares = document.querySelectorAll(".chem__CarouselDayContent");
+  if (elementCarousel) {
+    let chemSquares = document.querySelectorAll(".carousel__content");
     for (let i of chemSquares) {
-      i.classList.remove("chem__CarouselDayContent");
-      i.classList.add("chem__CarouselNightContent");
+      i.classList.remove("carousel__content--day");
+      i.classList.add("carousel__content--night");
     }
   }
 
-  if (homeTemplateAtomImage) {
-    homeTemplateAtomImage.style.backgroundColor = "white";
-    homeTemplateAtomImage.style.padding = "2rem";
-    homeTemplateAtomImage.style.borderRadius = "50%";
+  if (atomIcon) {
+    atomIcon.style.backgroundColor = "white";
+    atomIcon.style.padding = "2rem";
+    atomIcon.style.borderRadius = "50%";
   }
 
-  if (reactorTemplateSelectedOne && reactorTemplateSelectedTwo) {
-    reactorTemplateSelectedOne.style.backgroundColor = "white";
-    reactorTemplateSelectedOne.style.color = "black";
-    reactorTemplateSelectedTwo.style.color = "black";
-    reactorTemplateSelectedTwo.style.backgroundColor = "white";
+  if (dropdownOneSelected && dropdownTwoSelected) {
+    dropdownOneSelected.style.backgroundColor = "white";
+    dropdownOneSelected.style.color = "black";
+    dropdownTwoSelected.style.color = "black";
+    dropdownTwoSelected.style.backgroundColor = "white";
   }
 
-  if (chemTemplateArrowImage) {
-    chemTemplateArrowImage.style.backgroundColor = "white";
-    chemTemplateArrowImage.style.padding = "0 1rem";
-    chemTemplateArrowImage.style.borderRadius = "15%";
+  if (arrowIcon) {
+    arrowIcon.style.backgroundColor = "white";
+    arrowIcon.style.padding = "0 1rem";
+    arrowIcon.style.borderRadius = "15%";
   }
 
-  if (reactorTemplateDropdownOne && reactorTemplateDropdownTwo) {
-    reactorTemplateOptionsOne.style.backgroundColor =
-      "rgba(238, 238, 238, 0.9)";
-    reactorTemplateOptionsOne.style.color = "black";
-    reactorTemplateOptionsTwo.style.backgroundColor =
-      "rgba(238, 238, 238, 0.9)";
-    reactorTemplateOptionsTwo.style.color = "black";
-    reactorTemplateInputOne.style.border = "8px solid rgba(238, 238, 238, 0.9)";
-    reactorTemplateInputTwo.style.border = "8px solid rgba(238, 238, 238, 0.9)";
+  if (dropdownOneStatic && dropdownTwoStatic) {
+    dropdownOneOptions.style.backgroundColor = "rgba(238, 238, 238, 0.9)";
+    dropdownOneOptions.style.color = "black";
+    dropdownTwoOptions.style.backgroundColor = "rgba(238, 238, 238, 0.9)";
+    dropdownTwoOptions.style.color = "black";
+    dropdownOneInput.style.border = "8px solid rgba(238, 238, 238, 0.9)";
+    dropdownTwoInput.style.border = "8px solid rgba(238, 238, 238, 0.9)";
   }
 
-  baseTemplateBlock.style.backgroundColor = nightThemeColor;
+  baseTemplate.style.backgroundColor = nightTheme;
   window.localStorage.clear();
-  window.localStorage.setItem("theme", nightThemeColor);
+  window.localStorage.setItem("theme", nightTheme);
 };
 
-darkMoonImage.addEventListener("click", toggleTheme);
-lightMoonImage.addEventListener("click", toggleTheme);
+darkMoon.addEventListener("click", toggleTheme);
+lightMoon.addEventListener("click", toggleTheme);
+
